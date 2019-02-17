d403ef539d798943225b5f6985ef7be99e3ea91e
user <user@192.168.202.104>
2019-02-17 15:53:26 +0800

    201902171553

--- BradJava/src/tw/org/iii/myjava/PokerV2.java
+++ BradJava/src/tw/org/iii/myjava/PokerV2.java
@@ -1,5 +1,7 @@
 package tw.org.iii.myjava;
 
+import java.util.Arrays;
+
 public class PokerV2 {
 
 	public static void main(String[] args) {
@@ -22,17 +24,24 @@ public class PokerV2 {
 			}while (isDup);
 			
 			poker[i] = temp;	
-			System.out.println(poker[i]);
+			//System.out.println(poker[i]);
 		}
-		System.out.println("---");
+		//System.out.println("---");
 		
 		int[][] players = new int[4][13];
 		for(int i=0; i<poker.length; i++) {
 			players[i%4][i/4] = poker[i];
 		}
 		
-		for (int card : players[1]) {
-			System.out.print(card + " ");
+		String[] suits = {"¶Â®ç", "¬õ¤ß", "¤è¶ô", "±öªá"};
+		String[] values = {"A ","2 ","3 ","4 ","5 ","6 ","7 ",
+				"8 ","9 ","10","J ","Q ","K "};
+		for (int[] player : players) {
+			Arrays.sort(player);
+			for (int card : player) {
+				System.out.print(suits[card/13] + values[card%13] + "  ");
+			}
+			System.out.println();
 		}
 		
 		
