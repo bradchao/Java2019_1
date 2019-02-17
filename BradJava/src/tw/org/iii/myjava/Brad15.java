a1e0359b40fe23ff3e67c3c52d0ff6854d24a2b2
user <user@192.168.202.104>
2019-02-17 11:04:35 +0800

    2019021705

--- BradJava/src/tw/org/iii/myjava/Brad15.java
+++ BradJava/src/tw/org/iii/myjava/Brad15.java
@@ -3,13 +3,16 @@ package tw.org.iii.myjava;
 public class Brad15 {
 
 	public static void main(String[] args) {
-		for (int i=0; i<10; i++) {
-			if (i % 2 == 0) {
-				continue;
-			}
-			System.out.println(i);
-			if ( i == 7) {
-				break;
+		
+		brad: for(int j=10; j>0; j--) {
+			for (int i=0; i<10; i++) {
+				if (i % 2 == 0) {
+					continue;
+				}
+				System.out.println(i + ":" + j);
+				if ( i == 7 && j == 6) {
+					break brad;
+				}
 			}
 		}
 	}
