862b78e712f1041764662f16b642ae87bce64862
user <user@192.168.202.104>
2019-02-17 13:39:08 +0800

    201902171339

--- BradJava/src/tw/org/iii/myjava/Brad19.java
+++ BradJava/src/tw/org/iii/myjava/Brad19.java
@@ -4,9 +4,9 @@ public class Brad19 {
 
 	public static void main(String[] args) {
 		int[] p = new int[7];	// 0-6
-		for (int i=0; i<100; i++) {
-			int point = (int)(Math.random()*6+1);
-			p[point]++;
+		for (int i=0; i<100000; i++) {
+			int point = (int)(Math.random()*9+1);
+			p[point>=7?point-3:point]++;
 		}
 		for (int i=1; i<p.length; i++) {
 			System.out.printf("%d ¥X²{ %d ¦¸\n", i, p[i]);
