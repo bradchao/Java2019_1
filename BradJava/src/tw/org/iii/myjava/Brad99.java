d7983926e8707419a144fe86cf269ca2990afb65
user <user@192.168.202.104>
2019-02-17 10:28:41 +0800

    2019021703

--- BradJava/src/tw/org/iii/myjava/Brad99.java
+++ BradJava/src/tw/org/iii/myjava/Brad99.java
@@ -3,10 +3,13 @@ package tw.org.iii.myjava;
 public class Brad99 {
 
 	public static void main(String[] args) {
-		for (int j=1; j<=9; j=newLine(j)) {
-			for (int i=2; i<=5; i++) {
-				int result = i * j;
-				System.out.printf("%d x %d = %d\t", i, j, result);
+		for (int k=0; k<4; k=newLine(k)) {
+			for (int j=1; j<=9; j=newLine(j)) {
+				for (int i=2; i<=5; i++) {
+					int newi = i + k * 4;
+					int result = newi * j;
+					System.out.printf("%d x %d = %d\t", newi, j, result);
+				}
 			}
 		}
 	}
