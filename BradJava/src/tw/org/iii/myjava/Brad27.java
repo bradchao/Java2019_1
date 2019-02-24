48b0e67f8e90be5d7c726f2beeea676e425c242e
user <user@192.168.202.104>
2019-02-24 14:05:06 +0800

    201902241405

--- BradJava/src/tw/org/iii/myjava/Brad27.java
+++ BradJava/src/tw/org/iii/myjava/Brad27.java
@@ -1,10 +1,24 @@
 package tw.org.iii.myjava;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Brad27 extends JFrame {
+	private JButton btnOk, btnExit;
+	
 	public Brad27() {
 		super("°¸ªºµøµ¡«°¥«");
+		
+		btnOk = new JButton("Click Me");
+		btnExit = new JButton("Exit");
+		
+		setLayout(new FlowLayout());
+		
+		add(btnOk);
+		add(btnExit);
+		
 		setSize(640, 480);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
