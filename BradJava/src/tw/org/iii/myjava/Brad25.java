41638af94535af60604d4301d109953805ce9a07
user <user@192.168.202.104>
2019-02-24 13:26:33 +0800

    201902241326

---
+++ BradJava/src/tw/org/iii/myjava/Brad25.java
@@ -0,0 +1,17 @@
+package tw.org.iii.myjava;
+
+public class Brad25 {
+
+	public static void main(String[] args) {
+		Brad251 obj1 = new Brad251();
+	}
+
+}
+class Brad251 {
+	Brad251(){
+		m1();
+	}
+	void m1() {
+		System.out.println("Brad251:m1()");
+	}
+}
---
+++ BradJava/src/tw/org/iii/myjava/Brad26.java
@@ -0,0 +1,16 @@
+package tw.org.iii.myjava;
+
+import javax.swing.JFrame;
+
+public class Brad26 {
+	public static void main(String[] args) {
+		JFrame obj1 = new JFrame();
+		obj1.setSize(640, 480);
+		obj1.setVisible(true);
+		obj1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+
+//class MyWindow extends JFrame{
+//	
+//}
