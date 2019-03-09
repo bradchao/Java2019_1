6880b9db83ab4ea6178b14c8a74c9d3cdd33514d
user <user@192.168.202.104>
2019-03-09 09:35:46 +0800

    201903090935

---
+++ BradJava/src/tw/org/iii/myjava/Brad32.java
@@ -0,0 +1,19 @@
+package tw.org.iii.myjava;
+
+public class Brad32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Brad321 extends Object {
+	Brad321(int a){}
+}
+class Brad322 extends Brad321 {
+	Brad322(){
+		// super();
+		super(3);
+	}
+}
---
+++ BradJava/src/tw/org/iii/myjava/Brad33.java
@@ -0,0 +1,27 @@
+package tw.org.iii.myjava;
+
+public class Brad33 {
+
+	public static void main(String[] args) {
+		Brad331 obj1 = new Brad331();
+		obj1.m1();
+		obj1.m1(1);
+		obj1.m1(1f);
+		obj1.m1(1.2);
+		//obj1.m1((byte)1,(byte)1);
+	}
+
+}
+// Overload => ÂÐ¸ü
+// same class
+class Brad331 {
+	int m1() {System.out.println("()"); return 1;}
+	void m1(int a) {System.out.println("(int)");}
+	//void m1(float a){System.out.println("(float)");}
+	void m1(double a){System.out.println("(double)");}
+	void m1(int a, byte b){System.out.println("(int,byte)");}
+//	void m1(byte a, byte b){System.out.println("(byte,byte)");}
+	void m1(int a, int b){System.out.println("(int,int)");}
+	void m1(byte a, int b){System.out.println("(int,int)");}
+	
+}
