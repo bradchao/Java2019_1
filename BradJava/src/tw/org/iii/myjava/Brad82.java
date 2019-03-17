ce99519ed82ac294ac23f367f74961f114d71e10
user <user@192.168.202.104>
2019-03-17 14:33:08 +0800

    201903171433

--- BradJava/src/tw/org/iii/myjava/Brad82.java
+++ BradJava/src/tw/org/iii/myjava/Brad82.java
@@ -8,12 +8,24 @@ import java.nio.channels.ReadableByteChannel;
 public class Brad82 {
 
 	public static void main(String[] args) {
-		ByteBuffer buffer = ByteBuffer.allocate(8);
+		ByteBuffer buffer = ByteBuffer.allocate(4);
 		
 		try {
-			FileInputStream fin = new FileInputStream("");
+			FileInputStream fin = new FileInputStream("mytest/mydata.txt");
 			ReadableByteChannel channel = Channels.newChannel(fin);
 			
+			int bytes = channel.read(buffer);
+			System.out.println(bytes);
+			int d0 = buffer.get(0);
+			System.out.println((char)d0);
+			
+			buffer.flip();
+			
+			channel.read(buffer);
+			int d1 = buffer.get(1);
+			System.out.println((char)d1);
+			
+			channel.close();
 			
 			
 			
