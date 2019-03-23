4baeacd18447def484035041c53c141c304c6ebb
user <user@192.168.202.104>
2019-03-23 10:13:31 +0800

    201903231013

--- BradJava/src/tw/org/iii/myjava/MyView.java
+++ BradJava/src/tw/org/iii/myjava/MyView.java
@@ -4,13 +4,17 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
 public class MyView extends JPanel {
-	
 	public MyView() {
 		setBackground(Color.YELLOW);
+		MyMouseListener myMouseListener = new MyMouseListener();
+		addMouseListener(myMouseListener);
+		addMouseMotionListener(myMouseListener);
 	}
 
 	@Override
@@ -22,6 +26,27 @@ public class MyView extends JPanel {
 		g2d.setStroke(new BasicStroke(4));
 		g2d.drawLine(0, 0, 400, 400);
 		
+		
+	}
+	
+	private class MyMouseListener extends MouseAdapter {
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			super.mouseDragged(e);
+			System.out.println("dragged");
+		}
+		
+		@Override
+		public void mousePressed(MouseEvent e) {
+			super.mousePressed(e);
+			System.out.println("pressed");
+		}
+		 
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			super.mouseReleased(e);
+			System.out.println("released");
+		}
 		
 	}
 	
