594ea11f1d903578a83fef0ad07ff29b9d2bea9c
user <user@192.168.202.104>
2019-03-23 09:17:48 +0800

    201903230917

---
+++ BradJava/src/tw/org/iii/myjava/MySign.java
@@ -0,0 +1,39 @@
+package tw.org.iii.myjava;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MySign extends JFrame{
+	private JButton clear, undo, redo, saveJPEG, saveObj, loadObj;
+	
+	public MySign() {
+		super("簽名城市");
+		
+		setLayout(new BorderLayout());
+	
+		clear = new JButton("Clear");
+		undo = new JButton("Undo");
+		redo = new JButton("Redo");
+		
+		JPanel top = new JPanel(new FlowLayout());
+		top.add(clear);top.add(undo);top.add(redo);
+		
+		add(top, BorderLayout.NORTH);
+		
+		
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	
+	public static void main(String[] args) {
+		new MySign();
+	}
+
+}
