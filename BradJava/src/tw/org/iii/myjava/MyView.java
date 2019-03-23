31e3f6d1707fb1ec875f19bfe90deed023410e8e
user <user@192.168.202.104>
2019-03-23 09:47:45 +0800

    201903230947

--- BradJava/src/tw/org/iii/myjava/MyView.java
+++ BradJava/src/tw/org/iii/myjava/MyView.java
@@ -1,6 +1,9 @@
 package tw.org.iii.myjava;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -10,4 +13,16 @@ public class MyView extends JPanel {
 		setBackground(Color.YELLOW);
 	}
 
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		
+		g2d.setColor(Color.BLUE);
+		g2d.setStroke(new BasicStroke(4));
+		g2d.drawLine(0, 0, 400, 400);
+		
+		
+	}
+	
 }
