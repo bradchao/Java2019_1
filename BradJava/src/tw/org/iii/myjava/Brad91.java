d73d77f3aa4eb6d41ee57a98ebe1af769fad6bc3
user <user@192.168.202.104>
2019-03-24 10:48:58 +0800

    201903231048

--- BradJava/src/tw/org/iii/myjava/Brad91.java
+++ BradJava/src/tw/org/iii/myjava/Brad91.java
@@ -4,17 +4,33 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class Brad91 {
 	public static void main(String[] args) {
 		
+//		String sql = "INSERT INTO cust (cname,tel,birthday) VALUES ('brad3','123','1999-12-13')";
+		String sql = "DELETE from cust WHERE id = 2";
+//		String sql = "INSERT INTO cust (cname,tel,birthday) VALUES ('brad3','123','1999-12-13')";
+		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
+			
+//			Connection conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost/iii?user=root&password=root");
+			
+//			Connection conn = DriverManager.getConnection(
+//					"jdbc:mysql://localhost/iii", "root","root");
+			
+			Properties prop = new Properties();
+			prop.put("user", "root");
+			prop.put("password", "root");
+			
 			Connection conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost/iii?user=root&password=root");
+					"jdbc:mysql://localhost/iii", prop);
 			
 			Statement stmt = conn.createStatement();
-			stmt.execute("INSERT INTO cust (cname,tel,birthday) VALUES ('brad','123','1999-12-13')");
+			stmt.execute(sql);
 			conn.close();
 			
 			System.out.println("OK");
