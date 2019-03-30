0540a2f11726dcb4dda1d0b6aea29aeca0bf8b31
user <user@192.168.202.104>
2019-03-30 09:47:00 +0800

    201903300946

---
+++ BradJava/src/tw/org/iii/myjava/MyATM.java
@@ -0,0 +1,45 @@
+package tw.org.iii.myjava;
+
+public class MyATM {
+	public static void main(String[] args) {
+
+	}
+}
+class ATM {
+	private int limit = 1000;
+	private int money;
+	void add(int add) {
+		if (money + add > 1000) {
+			System.out.println("add:xxx");
+		}else {
+			money += add;
+			System.out.println("add:" + add + " => " + money);
+		}
+	}
+	void get(int get, String name) {
+		if (money < get) {
+			System.out.println("get:xxx");
+		}else {
+			money -= get;
+			System.out.println(name + " get:" + get + " => " + money);
+		}
+	}
+}
+class Bank extends Thread {
+	private ATM atm;
+	Bank(ATM atm){this.atm = atm;}
+	@Override
+	public void run() {
+		for (int i=0; i<10; i++) {
+			atm.add(500);
+			try {
+				Thread.sleep(1000);
+			}catch (Exception e) {
+			}
+		}
+	}
+}
+class Person extends Thread {
+	private ATM atm;
+	
+}
